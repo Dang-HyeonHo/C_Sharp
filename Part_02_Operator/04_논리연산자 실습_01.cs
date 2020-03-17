213a97e3f233f99e8b6ca54c448898f84a28250a
Hyeonho <60058645+Dang-HyeonHo@users.noreply.github.com>
2020-03-17 23:26:46 +0900

    Logical Operator ex_01

---
+++ Part_02_Operator/04_논리연산자 실습_01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_논리연산자_실습_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age = int.Parse(textBox1.Text);
+            string gender = textBox2.Text;
+
+            bool man = (5 <= age && age <= 15) && (gender == "남");
+            bool woman = (8 <= age && age <= 20) && (gender == "여");
+
+            bool dc = man || woman;
+
+            label4.Text = "이 사람은 할인 받을 수 있는 사람인가 : " + dc;
+        }
+    }
+}
