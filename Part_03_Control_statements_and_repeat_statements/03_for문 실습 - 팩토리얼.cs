a54274c1de845e5159d3dc998a6b42bdeaa0bf06
Hyeonho <60058645+Dang-HyeonHo@users.noreply.github.com>
2020-03-22 21:30:00 +0900

    for Repetitive Statement ex_02

---
+++ Part_03_Control_statements_and_repeat_statements/03_for문 실습 - 팩토리얼.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_if_for문_실습_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(textBox1.Text);
+
+            label2.Text = num + "! = ";
+
+            for (int i = num; i > 0; i--)
+                label2.Text += i + " X ";
+
+            label2.Text = label2.Text.TrimEnd(' ', 'X');
+        }
+    }
+}
