a3c6eed2bfe57a8340935a3dc026924a22feee0f
Hyeonho <60058645+Dang-HyeonHo@users.noreply.github.com>
2020-03-23 18:11:48 +0900

    if control statement ex_03

---
+++ Part_03_Control_statements_and_repeat_statements/07_if문 실습_03.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06_if_for문_실습_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age = int.Parse(textBox1.Text);
+
+            if (age < 15 || age >= 60)
+            {
+                if (age == 12)
+                {
+                    label2.Text = "반액할인";
+                }
+                else
+                {
+                    label2.Text = "할인혜택";
+                }
+            }
+            else
+            {
+                label2.Text = "할인혜택 없음";
+            }
+        }
+    }
+}
