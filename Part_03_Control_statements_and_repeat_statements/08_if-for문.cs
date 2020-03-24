03f5561b298c93f56d563d26cac89a44defeff0c
Hyeonho <60058645+Dang-HyeonHo@users.noreply.github.com>
2020-03-24 20:05:56 +0900

    if-for Statement

---
+++ Part_03_Control_statements_and_repeat_statements/08_if-for문.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08_if_for문_실습_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+
+            label2.Text = "";
+
+            for (i = 1; i <= 10; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    label2.Text += "짝 ";
+                }
+                else
+                {
+                    label2.Text += i.ToString() + " ";
+                }
+            }
+        }
+    }
+}
