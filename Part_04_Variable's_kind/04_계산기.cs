f743d35a3bad7d1fb076159b6220157336d31bcc
Hyeonho <60058645+Dang-HyeonHo@users.noreply.github.com>
2020-03-27 21:15:27 +0900

    Calculator

---
+++ Part_04_Variable's_kind/04_계산기.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_계산기
+{
+    public partial class Form1 : Form
+    {
+        double result = 0;
+               
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = result.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            try
+            {
+                double num = double.Parse(textBox1.Text);
+                result += num;  // 더하기
+                label1.Text = result.ToString();
+            }
+            catch(Exception ex)
+            {
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   
+            try
+            {
+                double num = double.Parse(textBox1.Text);
+                result -= num;  // 빼기
+                label1.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num = double.Parse(textBox1.Text);
+                result *= num;  //곱하기
+                label1.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num = double.Parse(textBox1.Text);
+
+                if (num != 0)
+                {
+                    result /= num;  //나누기
+                    label1.Text = result.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result = 0;     //초기화 (계산기 C ==)
+                label1.Text = result.ToString();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
