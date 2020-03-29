158275b61faaf44a13b4d2eb77ccf94df650f14c
Hyeonho <60058645+Dang-HyeonHo@users.noreply.github.com>
2020-03-29 21:53:41 +0900

    Switch control statement

---
+++ Part_03_Control_statements_and_repeat_statements/10_switch문.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace switch문
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(textBox1.Text);
+
+            switch (num)
+            {
+                case 0:
+                    label1.Text = "0입니다.";
+                    break;  // 반복문을 1회 벗어난다.
+                case 1:
+                    label1.Text = "1입니다.";
+                    break;
+                default:
+                    label1.Text = "0과 1은 아니네요.";
+                    break;
+            }
+        }
+    }
+}
