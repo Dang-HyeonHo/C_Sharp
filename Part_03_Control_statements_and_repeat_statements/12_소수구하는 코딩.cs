28c6c6c1b2872f1c522cd37621a2897af25dd014
Hyeonho <60058645+Dang-HyeonHo@users.noreply.github.com>
2020-06-02 23:33:27 +0900

    decimal

---
+++ Part_03_Control_statements_and_repeat_statements/12_소수구하는 코딩.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_소수구하는_코딩
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(textBox1.Text);
+                int count = 0;
+
+                for (int i = 1; i <= a; i++)
+                {
+                    if (a % i == 0)
+                        count++;
+                    if (count == 2)
+                        label1.Text = a + "는 소수입니다";
+                    else
+                        label1.Text = a + "는 소수가 아닙니다";
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+        }
+    }
+}
