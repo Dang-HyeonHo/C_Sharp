d9d54390d47c0df4129669bb886004d0d19f9755
Hyeonho <60058645+Dang-HyeonHo@users.noreply.github.com>
2020-06-06 23:42:03 +0900

    Array

---
+++ Part_05_Array_and_Random/04_배열.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_배열
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 배열의 방법
+
+            /* 방법 1                     ※ 가장 많이 선호하는 방법
+             * 
+             * int[] num = new int[4];
+             * 
+             * num[0] = N;  num[1] = N;
+             * num[2] = N;  num[3] = N;
+             * 
+             * 4 크기의 배열 - 0, 1, 2, 3  * 0부터 시작
+             */
+
+            /* 방법 2
+             * 
+             * int[] num = {N, N, N, N}
+             */
+
+            /* 방법 3
+             * 
+             * int [] num;
+             * num = new int[4]
+             * 
+             * num[0] = N;  num[1] = N;
+             * num[2] = N;  num[3] = N;
+             */
+        }
+    }
+}
